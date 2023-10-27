e44bca3314614157c95247c7823b13ff54631ea6
Maruf Abdirimov <marufabdirimov@studyportals.com>
2023-10-27 13:26:45 +0200

    feat: added a basic store

---
+++ src/stores/modal.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useModalStore = defineStore('modal', {
+	state: () => ({
+		isOpen: false,
+	}),
+	actions: {
+		openModal() {
+			this.isOpen = true
+		},
+		closeModal() {
+			this.isOpen = false
+		},
+	},
+	getters: {
+		hiddenClass(state) {
+			return state.isOpen === "true" ? "" : "hidden";
+		}
+	}
+})
