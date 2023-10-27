58e7fc96e2dcb87a43b7a939ce35f3ad086b0115
Maruf Abdirimov <marufabdirimov@studyportals.com>
2023-10-27 14:39:26 +0200

    feat: adding rules to the rest of the form

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,21 @@
-import { Field as VeeField, Form as VeeForm } from "vee-validate"
+import { Field as VeeField, Form as VeeForm, defineRule, ErrorMessage as VeeErrorMessage } from "vee-validate";
+import { required, min, max, alpha_spaces as alphaSpaces, email, numeric, confirmed, min_value, max_value } from "@vee-validate/rules";
 
 export default {
 	install(app) {
-		app.component("VeeField", VeeField)
-		app.component("VeeForm", VeeForm)
+		app.component("VeeField", VeeField);
+		app.component("VeeForm", VeeForm);
+		app.component("ErrorMessage", VeeErrorMessage);
+
+		defineRule("required", required);
+		defineRule("min", min);
+		defineRule("max", max);
+		defineRule("alphaSpaces", alphaSpaces);
+		defineRule("email", email);
+		defineRule("numeric", numeric);
+		defineRule("min_value", min_value);
+		defineRule("max_value", max_value);
+		defineRule("confirmed", confirmed);
+
 	}
 }
